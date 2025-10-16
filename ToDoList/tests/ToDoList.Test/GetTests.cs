02b2e999f9a54f6f4f2433c10a4a73ed3a088e80
Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>
2025-10-16 15:05:42 +0200

    feat: Code from lesson 4

--- ToDoList/tests/ToDoList.Test/GetTests.cs
+++ ToDoList/tests/ToDoList.Test/GetTests.cs
@@ -1,5 +1,7 @@
 namespace ToDoList.Test;
 
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.DTOs;
 using ToDoList.Domain.Models;
 using ToDoList.WebApi;
 
@@ -33,5 +35,53 @@ public class GetTests
 
         // Assert
         Assert.NotNull(value);
+    }
+
+    [Fact]
+    public void DeleteTest()
+    {
+        ToDoItemsController.items.Clear();
+
+        // Arrange
+        var controller = new ToDoItemsController();
+        ToDoItemsController.items.Add(new ToDoItem
+        {
+            ToDoItemId = 1,
+            Name = "ItemToDelete",
+            Description = "TestDelete",
+            IsCompleted = false
+        });
+
+        // Act
+        var result = controller.DeleteByid(1);
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+        Assert.DoesNotContain(ToDoItemsController.items, item => item.ToDoItemId == 1);
+    }
+
+    [Fact]
+    public void CreateTest()
+    {
+        ToDoItemsController.items.Clear();
+
+        // Arrange
+        var controller = new ToDoItemsController();
+        var request = new ToDoItemCreateRequestDto("addImte", "addDesc", true);
+
+        var actionResult = controller.Create(request);
+        var result = actionResult as CreatedAtActionResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(nameof(ToDoItemsController.ReadById), result.ActionName);
+
+        var dto = result.Value as ToDoItemGetResponseDto;
+        Assert.NotNull(dto);
+        Assert.Equal("addImte", dto.Name);
+        Assert.Equal("addDesc", dto.Description);
+        Assert.Equal(request.IsCompleted, dto.IsCompleted);
+        Assert.Equal(1, dto.Id);
+
+        Assert.Single(ToDoItemsController.items);
+        Assert.Equal(1, ToDoItemsController.items[0].ToDoItemId);
     }
 }
