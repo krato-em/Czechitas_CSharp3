f951312ef29a04f41b7b4c82c7331b7e262dec4d
krato-em <154600557+krato-em@users.noreply.github.com>
2025-10-21 21:15:41 +0200

    Lesson 5/databaze (#5)
    * Zadani ukolu Lekce 04
    
    * Po lekci 04 + ukol
    
    * Prezentace Lekce05
    
    * Code from lesson 5
    
    ---------
    
    Co-authored-by: Jiri Hudec <jirrick@outlook.cz>
    Co-authored-by: Martin Černil <m.cernil@gmail.com>
    Co-authored-by: Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>

---
+++ ToDoList/src/ToDoList.Persistence/ToDoItemsContext.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDoList.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Models;
+
+public class ToDoItemsContext : DbContext
+{
+    private readonly string connectionString;
+    public ToDoItemsContext(string connectionString = "DataSource=../../data/localdb.db") // connectionString - where the db is physically located
+    {
+        this.connectionString = connectionString;
+        this.Database.Migrate();
+    }
+
+    public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(connectionString);
+    }
+}
