4cbf704eb97caf3435c72b7d056c5d48ca79cc7c
krato-em <154600557+krato-em@users.noreply.github.com>
2025-11-25 18:21:30 +0100

    feat: sorting buttons added (#11)
    Co-authored-by: Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>

--- ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
@@ -1,11 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 namespace ToDoList.Frontend.Views;
-public record ToDoItemView
-{
-    public required int ToDoItemId { get; set; }
-    [Length(1, 50)]
-    public required string Name { get; set; }
-    [StringLength(250)]
-    public required string Description { get; set; }
-    public required bool IsCompleted { get; set; }
-}
+// public record ToDoItemView
+// {
+//     public required int ToDoItemId { get; set; }
+//     [Length(1, 50)]
+//     public required string Name { get; set; }
+//     [StringLength(250)]
+//     public required string Description { get; set; }
+//     public required bool IsCompleted { get; set; }
+// }
+
+public record ToDoItemView(
+    int ToDoItemId,
+    string Name,
+    string Description,
+    bool IsCompleted
+);
