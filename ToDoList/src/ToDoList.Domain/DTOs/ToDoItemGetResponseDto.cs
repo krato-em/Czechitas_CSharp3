a082c8acd418db894fc8fea65cf13aaad48ce324
krato-em <154600557+krato-em@users.noreply.github.com>
2025-11-29 15:53:17 +0100

    lesson-9: httpclient - be and fe connection (#12)
    Co-authored-by: Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>

--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
+++ ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
@@ -1,8 +1,8 @@
 namespace ToDoList.Domain.DTOs;
 
+using System;
 using ToDoList.Domain.Models;
-
-public record class ToDoItemGetResponseDto(int Id, string Name, string Description, bool IsCompleted) //let client know the Id
+public record ToDoItemGetResponseDto(int Id, string Name, string Description, bool IsCompleted) //let client know the Id
 {
     public static ToDoItemGetResponseDto FromDomain(ToDoItem item) => new(item.ToDoItemId, item.Name, item.Description, item.IsCompleted);
 }
