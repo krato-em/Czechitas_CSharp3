b22eed1b5c2db70bd0a741302f9c0543a0686e0c
Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>
2025-12-07 18:55:27 +0100

    Lesson-10: Edit ToDoItem page created

--- ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -16,13 +16,34 @@ public class ToDoItemsClient : IToDoItemsClient
         var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems"); // v tenhle krok potrebujeme vylozene pockat na vysledek - nechceme jit dal dokud nemame odpoved -> proto 'await'
         // to 'await' zpusobuje i to, ze se nam nevraci Response, ale vraci se nam List<>
 
-        toDoItemViews = response.Select(dto => new ToDoItemView(
-            dto.Id,
-            dto.Name,
-            dto.Description,
-            dto.IsCompleted
-            )).ToList();
+        toDoItemViews = response.Select(dto => new ToDoItemView()
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Description = dto.Description,
+            IsCompleted = dto.IsCompleted
+        }).ToList();
 
         return toDoItemViews;
+    }
+    public async Task<ToDoItemView?> ReadItemByIdAsync(int itemId)
+    {
+        var response = await httpClient.GetFromJsonAsync<ToDoItemGetResponseDto>($"api/ToDoItems/{itemId}");
+
+        var toDoItem = new ToDoItemView()
+        {
+            Id = response.Id,
+            Name = response.Name,
+            Description = response.Description,
+            IsCompleted = response.IsCompleted
+        };
+        return toDoItem;
+    }
+
+    public async Task UpdateItemAsync(ToDoItemView item)
+    {
+        //TODO: zabalit to do Try/Catch
+        var itemRequest = new ToDoItemUpdateRequestDto(item.Name, item.Description, item.IsCompleted);
+        var response = await httpClient.PutAsJsonAsync($"api/ToDoItems/{item.Id:int}", itemRequest);
     }
 }
