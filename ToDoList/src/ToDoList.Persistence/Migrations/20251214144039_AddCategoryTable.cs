49b229865f2b1f9d31cf369e8538b09806072635
Emilie Kratochvilova <Emilie.Kratochvilova@solarwinds.com>
2025-12-14 15:46:09 +0100

    Working migrations added

---
+++ ToDoList/src/ToDoList.Persistence/Migrations/20251214144039_AddCategoryTable.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "ToDoItems",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoItems_CategoryId",
+                table: "ToDoItems",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDoItems_Categories_CategoryId",
+                table: "ToDoItems",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDoItems_Categories_CategoryId",
+                table: "ToDoItems");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDoItems_CategoryId",
+                table: "ToDoItems");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "ToDoItems");
+        }
+    }
+}
